84fcce5a0133dad441ed78909da91678fca5809f
J-Mwema <joseph.mwemake@gmail.com>
2026-01-24 21:05:01 +0300

    cherry: bring fixes from feature/jwt-auth (serializers, urls, applications)

---
+++ backend/applications/views.py
@@ -0,0 +1,21 @@
+from rest_framework import generics, permissions
+from .models import Application
+from .serializers import (
+    ApplicationCreateSerializer,
+    ApplicatioinStatusUpdateSerializer,
+)
+from .permissions import IsEmployerAndJobOwner
+from users.models import User
+
+class ApplicationCreateView(generics.CreateAPIView):
+    serializer_class = ApplicationCreateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(applicant=self.request.user)
+
+
+class ApplicationStatusUpdateView(generics.UpdateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicatioinStatusUpdateSerializer
+    permission_classes = [IsEmployerAndJobOwner]
