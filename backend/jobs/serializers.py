e82a8564a9b55cd8d5a5c8caa6e06e7e9653aad2
J-Mwema <joseph.mwemake@gmail.com>
2026-02-01 21:05:15 +0300

    feat(jobs): add Industry and JobType serializers; update JobSerializer to accept industry/job_type ids

--- backend/jobs/serializers.py
+++ backend/jobs/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
-from .models import Job
+from .models import Job, Industry, JobType
+
+
+class IndustrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Industry
+        fields = ('id', 'name')
+
+
+class JobTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobType
+        fields = ('id', 'name')
 
 
 class JobSerializer(serializers.ModelSerializer):
     employer = serializers.ReadOnlyField(source='employer.username')
+
+    industry = IndustrySerializer(read_only=True)
+    industry_id = serializers.PrimaryKeyRelatedField(
+        queryset=Industry.objects.all(), source='industry', write_only=True, required=False, allow_null=True
+    )
+
+    job_type = JobTypeSerializer(read_only=True)
+    job_type_id = serializers.PrimaryKeyRelatedField(
+        queryset=JobType.objects.all(), source='job_type', write_only=True, required=False, allow_null=True
+    )
 
     class Meta:
         model = Job
