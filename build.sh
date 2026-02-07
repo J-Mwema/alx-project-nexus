3a25a797c5117a1502d83c624fbccf8e6307b551
J-Mwema <joseph.mwemake@gmail.com>
2026-02-07 19:59:44 +0300

    Fix superuser check in build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
-if not User.objects.filter(username='admin').exists():
+if not User.objects.filter(username='gikonyo').exists():
     User.objects.create_superuser('gikonyo', 'gikonyo.mwema@gmail.com', 'Gikonyo!')
     print('Superuser created')
 "
